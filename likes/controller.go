7208781c36891bbf2a5ffbd0599c761c96f50610
Mykyta Paliienko <floyernick@gmail.com>
2020-06-29 16:49:39 +0300

    Add delete like use case

--- likes/controller.go
+++ likes/controller.go
@@ -7,4 +7,5 @@ import (
 
 func RegisterRoutes(mux *http.ServeMux, db database.DB) {
 	mux.HandleFunc("/likes.create", LikesCreateController{db}.Handler)
+	mux.HandleFunc("/likes.delete", LikesDeleteController{db}.Handler)
 }
